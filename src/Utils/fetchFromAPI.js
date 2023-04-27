1c3c75f672fd18b8b8d11c708994be298bb42504
Raunakp21 <pandeyraunak64@gmail.com>
2023-04-27 11:24:26 +0530

    Initial commit

---
+++ src/Utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "content-type": "application/octet-stream",
+    "X-RapidAPI-Key": "f95f2f0afamsh0bc625fbc38b1ebp1efb71jsne6b4c3686169",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFormAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
