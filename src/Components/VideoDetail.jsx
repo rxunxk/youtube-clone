2089673b166ffeaa7518388b3a8a4864287c9fab
Raunakp21 <pandeyraunak64@gmail.com>
2023-04-27 16:51:34 +0530

    Vide player bug fix

--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -71,14 +71,15 @@ const VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
-        <Box
+        {/* <Box
           px={2}
           py={{ md: 1, xs: 5 }}
           justifyContent="center"
           alignItems="center"
         >
           <Videos videos={videos} direction="column" />
-        </Box>
+        </Box> */}
+        <Videos videos={videos} direction="column" />
       </Stack>
     </Box>
   );
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
-
-const Videos = ({ videos }) => {
+import { Loader } from "./";
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
   return (
-    <Stack flexDirection="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, index) => (
-        <Box key={index}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
